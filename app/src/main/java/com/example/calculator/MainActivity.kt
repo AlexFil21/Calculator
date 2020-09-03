12d46cf08849f9136b5adec41f06fd51cc4bd54e
AlexFil21 <alexey.filatov21@gmail.com>
2020-09-03 12:08:07 +0300

    mvvm1

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.calculator
-import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.calculator.viewModel.MyViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,41 +27,6 @@ class MainActivity : AppCompatActivity() {
             textField.text = it
         })
     }
-
-
-//    fun changeRes (tV: TextView) {
-//        if (sum.equals("")) {
-//            textField.text = tV.text;
-//            sum = textField.text.toString();
-//        } else {
-//            textField.text = textField.text.toString() + tV.text;
-//            sum = textField.text.toString();
-//        }
-//    }
-//
-//    fun resetData () {
-//        textField.text = "0"
-//        sum = ""
-//        res.clear()
-//    }
-//
-//    fun getSum () {
-//        res.add(textField.text as String)
-//        textField.text = "+"
-//        sum = ""
-//    }
-//
-//    fun result (){
-//        var i:Long = 0
-//        res.add(textField.text as String)
-//
-//        for (key in res) {
-//            i += key.toInt()
-//        }
-//
-//        textField.text = i.toString()
-//        res.clear()
-//    }
 
 
 }
